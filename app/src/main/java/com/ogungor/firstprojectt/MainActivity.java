b0326ae4fceef10ce7fba97963a53d265e544fe7
oguzhangungor <oguzhan.gngr91@gmail.com>
2020-08-06 21:29:24 +0300

    Show Next-Back Image

--- app/src/main/java/com/ogungor/firstprojectt/MainActivity.java
+++ app/src/main/java/com/ogungor/firstprojectt/MainActivity.java
@@ -4,25 +4,47 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
 
-import org.w3c.dom.Text;
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    int mImageNumber = -1;
+    ImageView mImageView;
+    ArrayList<Integer> mimageList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mImageView = findViewById(R.id.imageView);
+        mimageList.add(R.drawable.cr71);
+        mimageList.add(R.drawable.cr72);
+        mimageList.add(R.drawable.cr73);
     }
 
-    public void btnText(View view)
-    {
-
-        ImageView imageView= findViewById(R.id.imageView);
-        imageView.setImageResource(R.drawable.cr72);
-        Button button=findViewById(R.id.button);
-        button.setText(getString(R.string.change_btn_text));
+    public Integer getImage(int placeNumber, ArrayList<Integer> imageList) {
+        return imageList.get(placeNumber);
     }
+
+    public void btnNext(View view) {
+
+        if (mImageNumber == 2)
+            Toast.makeText(this, "Son Resim.", Toast.LENGTH_LONG).show();
+        else {
+            mImageNumber++;
+            mImageView.setImageResource(getImage(mImageNumber, mimageList));
+        }
+    }
+
+    public void btnBack(View view) {
+        if (mImageNumber == 0)
+            Toast.makeText(this, "Son Resim.", Toast.LENGTH_LONG).show();
+        else {
+            mImageNumber--;
+            mImageView.setImageResource(getImage(mImageNumber, mimageList));
+        }
+    }
+
 }
